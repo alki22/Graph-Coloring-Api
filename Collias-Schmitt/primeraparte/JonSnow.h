fd4f9487fbe4410cca425ee3e4523428fccd1837
alki22 <alki.collias@gmail.com>
2017-04-27 12:16:59 -0300

    Agregadas nuevas dependencias

--- Collias-Schmitt/primeraparte/JonSnow.h
+++ Collias-Schmitt/primeraparte/JonSnow.h
@@ -3,7 +3,21 @@ Integrantes:
 Federico Schmitt - e-mail: kolo_schmitt@gmail.com
 Theo Alkibiades Collias - e-mail: alki.collias@gmail.com
 */
+
+#ifndef JONSNOW_H_
+#define JONSNOW_H_
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef unsigned int *u32;
+#include "Vertice.c"
+
+typedef unsigned int u32;
+
+
+
+
+
+#endif 
